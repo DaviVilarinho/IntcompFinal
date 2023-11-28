d35d08f892fd66ebd88dc956e59e948b3c83d5b9
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-11-28 15:12:00 -0300

    general improvements

--- src/AllFeatures.jl
+++ src/AllFeatures.jl
@@ -9,50 +9,51 @@ using CategoricalArrays
 using DataFrames
 using JLD2
 
+const CARDINAL_DICT = Dict("N" => 0.0, "NNE" => π / 8, "NE" => π / 4, "ENE" => 3 * π / 8,
+  "E" => π / 2, "ESE" => 5 * π / 8, "SE" => 3 * π / 4, "SSE" => 7 * π / 8,
+  "S" => π, "SSW" => -7 * π / 8, "SW" => -3 * π / 4, "WSW" => -5 * π / 8,
+  "W" => -π / 2, "WNW" => -3 * π / 8, "NW" => -π / 4, "NNW" => -π / 8)
 function cardinalToRadians(cardinal_point::Union{AbstractString,Missing,String})::Union{Float64,Missing}
   if ismissing(cardinal_point) || cardinal_point == "NA"
     return 0
   end
-  cardinal_dict = Dict("N" => 0.0, "NNE" => π / 8, "NE" => π / 4, "ENE" => 3 * π / 8,
-    "E" => π / 2, "ESE" => 5 * π / 8, "SE" => 3 * π / 4, "SSE" => 7 * π / 8,
-    "S" => π, "SSW" => -7 * π / 8, "SW" => -3 * π / 4, "WSW" => -5 * π / 8,
-    "W" => -π / 2, "WNW" => -3 * π / 8, "NW" => -π / 4, "NNW" => -π / 8)
 
-  return get(cardinal_dict, cardinal_point, NaN)
+  return get(CARDINAL_DICT, cardinal_point, NaN)
 end
 
+const COLUMN_TYPES = Dict(
+  :Date => String,
+  :Location => String,
+  :MinTemp => Float64,
+  :MaxTemp => Float64,
+  :Rainfall => Float64,
+  :Evaporation => Float64,
+  :Sunshine => Float64,
+  :WindGustSpeed => Float64,
+  :WindDir9am => String,
+  :WindGustDir => String,
+  :WindDir3pm => String,
+  :WindSpeed9am => Float64,
+  :WindSpeed3pm => Float64,
+  :Humidity9am => Float64,
+  :Humidity3pm => Float64,
+  :Pressure9am => Float64,
+  :Pressure3pm => Float64,
+  :Cloud9am => Float64,
+  :Cloud3pm => Float64,
+  :Temp9am => Float64,
+  :Temp3pm => Float64,
+  :RainToday => String,
+  :RainTomorrow => String
+)
+
 function readAndProcessData(lines::Int64=0)
-  column_types = Dict(
-    :Date => String,
-    :Location => String,
-    :MinTemp => Float64,
-    :MaxTemp => Float64,
-    :Rainfall => Float64,
-    :Evaporation => Float64,
-    :Sunshine => Float64,
-    :WindGustSpeed => Float64,
-    :WindDir9am => String,
-    :WindGustDir => String,
-    :WindDir3pm => String,
-    :WindSpeed9am => Float64,
-    :WindSpeed3pm => Float64,
-    :Humidity9am => Float64,
-    :Humidity3pm => Float64,
-    :Pressure9am => Float64,
-    :Pressure3pm => Float64,
-    :Cloud9am => Float64,
-    :Cloud3pm => Float64,
-    :Temp9am => Float64,
-    :Temp3pm => Float64,
-    :RainToday => String,
-    :RainTomorrow => String
-  )
-  dataframe = DataFrame(CSV.File("weatherAUS.csv", missingstring=["NA"], dateformat=Dates.ISODateFormat, types=column_types))
+  dataframe = DataFrame(CSV.File("weatherAUS.csv", missingstring=["NA"], dateformat=Dates.ISODateFormat, types=COLUMN_TYPES))
 
   if lines > 0
     dataframe = dataframe |> x -> first(x, lines)
   end
-  dataframe = select(dataframe, Not([:Evaporation, :Sunshine])) # describe mostra que tudo é NA
+  dataframe = select(dataframe, Not([:Evaporation, :Sunshine, :Cloud9am, :Cloud3pm])) # describe mostra que metade é NA
   dropmissing!(dataframe)
 
   for directionColumn in [:WindDir3pm, :WindDir9am, :WindGustDir]
@@ -66,7 +67,7 @@ function readAndProcessData(lines::Int64=0)
   end
   dataframe.Location = map(l -> get(locDict, l, 0), dataframe.Location)
 
-  dataframe.Date = map(d -> datetime2unix(DateTime(d)), dataframe.Date)
+  dataframe.Date = datetime2unix.(DateTime.(dataframe.Date))
 
   dataframe.RainToday .= coalesce.(dataframe.RainToday, "No")
   dataframe.RainTomorrow .= coalesce.(dataframe.RainTomorrow, "No")
@@ -112,22 +113,24 @@ function main(fun::Function=σ, inputdata::Int64=145460, epoch::Int64=150)
     softmax
   )
 
-  optim = Flux.setup(Flux.Adam(0.001), model)
+  optim = Flux.setup(Flux.Adam(2e-8), model)
 
+
+  println("época,média de loss")
   for e in 1:epoch
-    losses = []
-    for i in 1:size(x_train, 1)
-      x_t = collect(x_train[i, :])
-      y_t = y_train.RainTomorrow[i]
-      e_loss, grads = Flux.withgradient(model) do predict
-        y_hat = predict(x_t)
-        Flux.crossentropy(y_hat, y_t)
+    @time begin
+      losses = []
+      for i in 1:size(x_train, 1)
+        x_t = collect(Float32, x_train[i, :])
+        y_t = y_train.RainTomorrow[i]
+        e_loss, grads = Flux.withgradient(model) do predict
+          y_hat = predict(x_t)
+          Flux.crossentropy(y_hat, y_t)
+        end
+        Flux.update!(optim, model, grads[1])
+        push!(losses, e_loss)
       end
-      Flux.update!(optim, model, grads[1])
-      push!(losses, e_loss)
-    end
-    if e % ceil(epoch / 10) == 0
-      println("Época: $e, loss: ", mean(losses))
+      println("$e,", mean(losses))
     end
   end
 
@@ -152,6 +155,6 @@ function main(fun::Function=σ, inputdata::Int64=145460, epoch::Int64=150)
     @save "./models/$nowmoment-E$epoch-MLP$mlp_hidden-$inputdata-$n_correct-out-of-$n_test.bson" model
     jldsave("./models/$nowmoment-E$epoch-MLP$mlp_hidden-$inputdata-$n_correct-out-of-$n_test.jld2"; model_state)
   end
-  return model, data
+  return model, data, x_test, y_test
 end
 end
